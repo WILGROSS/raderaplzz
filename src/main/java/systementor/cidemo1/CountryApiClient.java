4693f7096aed459e8ec6bd29eb8b3f369a3ce787
WilliamGross <william@gross.nu>
2025-12-02 22:49:51 +0100

    Add Country service

---
+++ src/main/java/systementor/cidemo1/CountryApiClient.java
@@ -0,0 +1,5 @@
+package systementor.cidemo1;
+
+public interface CountryApiClient {
+    Country fetchCountryByName(String name);
+}
---
+++ src/main/java/systementor/cidemo1/CountryService.java
@@ -0,0 +1,55 @@
+package systementor.cidemo1;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CountryService {
+
+    private final CountryApiClient apiClient;
+
+    public CountryService(CountryApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    public List<String> getSortedLanguages(String name) {
+        Country country = apiClient.fetchCountryByName(name);
+
+        return country.getLanguages()
+                .values()
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getSortedBorders(String name) {
+        Country country = apiClient.fetchCountryByName(name);
+
+        return country.getBorders()
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public String getRegionInfo(String name) {
+        Country country = apiClient.fetchCountryByName(name);
+        return country.getRegion() + " / " + country.getSubregion();
+    }
+
+    public boolean isHighlyPopulated(String name) {
+        Country country = apiClient.fetchCountryByName(name);
+        return country.getPopulation() > 10000000;
+    }
+
+    public double getPopulationDensity(String name) {
+        Country country = apiClient.fetchCountryByName(name);
+        if (country.getArea() <= 0) {
+            throw new IllegalArgumentException("Area must be greater than 0");
+        }
+        return (double) country.getPopulation() / country.getArea();
+    }
+
+    public String getGoogleMapsUrl(String name) {
+        Country country = apiClient.fetchCountryByName(name);
+        return country.getGoogleMapsUrl();
+    }
+}
