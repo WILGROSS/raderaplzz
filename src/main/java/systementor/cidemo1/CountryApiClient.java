6799a3a55208bc61b1009d2de97a0c7191a27258
WilliamGross <william@gross.nu>
2025-12-05 17:25:13 +0100

    Reset files for MOCK excercise

--- src/main/java/systementor/cidemo1/CountryApiClient.java
+++ src/main/java/systementor/cidemo1/CountryApiClient.java
@@ -1,51 +1,8 @@
 package systementor.cidemo1;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-public class CountryApiClient {
-    private static final Country SWEDEN = new Country(
-            "Latvia",
-            "Republic of Latvia",
-            "Latvija",
-            "Latvijas Republikas",
-            "Riga",
-            "Europe",
-            "Northern Europe",
-            Map.of(
-                    "sve", "Swedish",
-                    "eng", "English"
-            ),
-            List.of("NOR", "FIN"),
-            1829000,
-            64559.0,
-            "https://goo.gl/maps/iQpUkH7ghq31ZtXe9"
-    );
-
-    private static final Country GERMANY = new Country(
-            "Deutschland",
-            "Deutschland",
-            "Germany",
-            "Germany",
-            "Berlin",
-            "Europe",
-            "Central Europe",
-            Map.of(
-                    "deu", "German",
-                    "eng", "English"
-            ),
-            List.of("POL", "BEL", "LUX", "CZH","FRA"),
-            123456,
-            6789113,
-            "https://goo.gl/maps/iQpUkH7ghq31ZtXe9"
-    );
-    public Country fetchCountryByName(String name){
-
-        if(Objects.equals(name, "Sweden")){
-            return SWEDEN;
-        }
-
-        return GERMANY;
-    }
+public interface CountryApiClient {
+    public Country fetchCountryByName(String name);
 }
--- src/test/java/systementor/cidemo1/CountryServiceTest.java
+++ src/test/java/systementor/cidemo1/CountryServiceTest.java
@@ -1,111 +1,17 @@
 package systementor.cidemo1;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Logger;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.Mockito.when;
-
-@ExtendWith({MockitoExtension.class})
 public class CountryServiceTest {
 
-    CountryApiClient notMockedApiClient = new CountryApiClient();
-    CountryService notMockedCountryservice = new CountryService(notMockedApiClient);
-
-    @Mock
-    CountryApiClient apiClient;
-
-    @InjectMocks
-    CountryService countryService;
-
-    private final static Logger logger = Logger.getLogger(CountryServiceTest.class.getName());
-
-    private static final Country LATVIA = new Country(
-            "Latvia",
-            "Republic of Latvia",
-            "Latvija",
-            "Latvijas Republikas",
-            "Riga",
-            "Europe",
-            "Northern Europe",
-            Map.of(
-                    "lav", "Latvian",
-                    "eng", "English"
-            ),
-            List.of("EST", "LTU", "RUS", "BLR"),
-            1829000,
-            64559.0,
-            "https://goo.gl/maps/iQpUkH7ghq31ZtXe9"
-    );
-
-
     @Test
-    void getSortedLanguagesReturnsAlphabeticallySortedLangues() {
-        logger.info("Starting test getSortedLanguagesReturnsAlphabeticallySortedLangues...");
-
-        var result = notMockedCountryservice.getSortedLanguages("Sweden");
-        logger.info("Result from getSortedLanguages: " + result);
-
-        assertEquals(List.of("English", "Swedish"), result);
-    }
-
-
-    @Test
-    void getSortedBordersReturnsAlphabeticallySortedNeighbouringCountries() {
-        logger.info("Starting test getSortedBorders returns alphabetically sorted bordering countries");
-        when(apiClient.fetchCountryByName("Latvia"))
-                .thenReturn(LATVIA);
+    void getSortedLaungagesReturnsAlphabeticallySortedLangues() {
+        when(countryApiClient.fetchCountryByName("Latvia"))
+            .thenReturn(LATVIA);
 
         var result = countryService.getSortedBorders("Latvia");
 
-        logger.info("Result from getSortedBorders: " + result.toString());
-        assertEquals(List.of("BLR", "EST", "LTU", "RUS"), result);
-    }
-
-    @Test
-    void isHighlyPopulatedReturnsFalseWhenPopulationIsBelowTenMillion() {
-        logger.info("Starting test isHighlyPopulatedReturnsFalseWhenPopulationIsBelowTenMillion");
-
-        when(apiClient.fetchCountryByName("Latvia"))
-                .thenReturn(LATVIA);
-
-        var result = countryService.isHighlyPopulated("Latvia");
-
-        logger.info("Result from isHighlyPopulated: " + result);
-        assertFalse(result);
-    }
-
-    @Test
-    void getPopulationDensityReturnsCorrectValue() {
-        logger.info("Starting test getPopulationDensityReturnsCorrectValue");
-        when(apiClient.fetchCountryByName("Latvia"))
-                .thenReturn(LATVIA);
-
-        var result = countryService.getPopulationDensity("Latvia");
-
-        logger.info("Result from getPopulationDensity: " + result);
-        assertEquals(LATVIA.getPopulation() / LATVIA.getArea(), result);
-    }
-
-
-    @Test
-    void getGoogleMapsUrlReturnsCorrectGoogleMapsUrl() {
-        logger.info("Starting test getGoogleMapsUrl returns correct GoogleMapsUrl");
-        when(apiClient.fetchCountryByName("Latvia"))
-                .thenReturn(LATVIA);
-
-        var result = countryService.getGoogleMapsUrl("Latvia");
-        logger.info("Result from getGoogleMapsUrl: " + result);
-
-        assertEquals(LATVIA.getGoogleMapsUrl(), result);
+        assertEquals(List.of("English", "Latvian"), result);
     }
 
 }
